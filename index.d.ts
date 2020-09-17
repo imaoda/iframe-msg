3caf742f26e2f3c7f661a531aabdc309033d100c
wangyongfeng@kuaishou.com <imaoda@126.com>
2020-09-17 20:05:20 +0800

    feat: update

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,15 @@
 type Cb = (data: any, cb: (msg?: any) => void | any) => void | any;
 
-function setDefaultSon(name: string | HTMLIFrameElement): void {}
+export function setDefaultSon(name: string | HTMLIFrameElement): void;
 
-function setMsgHandler(fn: any): void {}
+export function setMsgHandler(fn: any): void;
 
-function sendSon(data: any, name?: string | HTMLIFrameElement): Promise<any> {}
+export function sendSon(
+  data: any,
+  name?: string | HTMLIFrameElement
+): Promise<any>;
 
-function sendDad(data: any, name?: string | HTMLIFrameElement): Promise<any> {}
-
-export default { setDefaultSon, setMsgHandler, sendSon, sendDad };
+export function sendDad(
+  data: any,
+  name?: string | HTMLIFrameElement
+): Promise<any>;
